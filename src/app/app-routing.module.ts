058b8fa2dddb75ff524abda780773f50b4b7eae0
Lilla624 <vargalilla@ktch.hu>
2024-12-19 15:11:34 +0100

    kezdes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // A főoldal
+  { path: 'products', component: ProductsComponent }, // Termékek listája
+  { path: 'navbar', component: NavbarComponent }, // Navbar oldal
+  { path: 'login', component: LoginComponent }, // Belépési oldal
+  { path: '**', redirectTo: '' } // Ha nincs érvényes útvonal, visszairányít a főoldalra
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    // Keresés ikon eseménykezelő
+    const searchIcon = document.querySelector('.search-icon');
+    if (searchIcon) {
+      searchIcon.addEventListener('click', () => {
+        const searchBar = document.querySelector('.search-bar') as HTMLElement;
+        searchBar.classList.toggle('active');
+      });
+    }
+
+    // Lenyitható menü kezelése
+    const menuIcon = document.querySelector('.menu-icon');
+    if (menuIcon) {
+      menuIcon.addEventListener('click', () => {
+        const dropdownMenu = document.querySelector('.dropdown-menu') as HTMLElement;
+        dropdownMenu.classList.toggle('show');
+      });
+    }
+  }
+}
+
+
+
